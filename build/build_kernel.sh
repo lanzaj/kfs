cd20e4adfe50c59f7a7f5793ece805e804c7174a
thibQut <grasset.thibaut@gmail.com>
2024-04-29 17:08:03 +0200

    kernel now running in 64 bit

--- build/build_kernel.sh
+++ build/build_kernel.sh
@@ -2,9 +2,11 @@
 
 nasm -f elf64 src/assembly/multiboot_header.asm
 nasm -f elf64 src/assembly/boot.asm
+nasm -f elf64 src/assembly/long_mode_init.asm
 ld -n -o kfs.bin -T src/assembly/linker.ld \
     src/assembly/multiboot_header.o \
-    src/assembly/boot.o
+    src/assembly/boot.o \
+    src/assembly/long_mode_init.o
 
 mv kfs.bin ./isofiles/boot/kernel.bin
 
--- src/assembly/boot.asm
+++ src/assembly/boot.asm
@@ -1,15 +1,56 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
 start:
     ; update stack pointer so we're ready to call functions
     mov esp, stack_top
+    call check_multiboot
     call check_cpuid
     call check_long_mode
-    mov dword [0xb8000], 0x2f4b2f4f  
-    hlt
+    call set_up_page_tables
+    call enable_paging
+    lgdt [gdt64.pointer]
+    jmp gdt64.code:long_mode_start
 
+enable_paging:
+    ; load p4 address to cr3 register (cpu uses this register to access p4 table)
+    mov eax, p4_table
+    mov cr3, eax
+    ; enable Physical Address Extension flag in cr4
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+    ; set the long mode bit in the EFER Model Specific Register
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+    ; enable paging in the cr0 register
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+    ret
+set_up_page_tables:
+    mov eax, p3_table ; map first p4 entry to p3 table
+    or eax, 0b11 ; present + writable
+    mov [p4_table], eax
+
+    mov eax, p2_table ; same for p3 and p2
+    or eax, 0b11
+    mov [p3_table], eax
+    mov ecx, 0  ; index
+
+.map_p2_table: ; map each P2 entry to a 2MiB page (loop)
+    mov eax, 0x200000 ; 2MiB
+    mul ecx ; index * 2MiB to get the start address of each page
+    or eax, 0b10000011 ; present + writable + huge
+    mov [p2_table + ecx * 8], eax ;
+    inc ecx ; index++
+    cmp ecx, 512 ; P2 has 512 entry
+    jne .map_p2_table ; loop back
+    ret
 check_multiboot:
     ; check the bootloader wrote its magic value in eax before loading our kernel
     cmp eax, 0x36d76289
@@ -85,6 +126,22 @@ error:
     hlt
 
 section .bss
+align 4096
+p4_table:
+    resb 4096
+p3_table:
+    resb 4096
+p2_table:
+    resb 4096
 stack_bottom:
     resb 64
 stack_top:
+
+section .rodata
+gdt64:
+    dq 0 ; zero entry
+.code: equ $ - gdt64 
+    dq (1<<43) | (1<<44) | (1<<47) | (1<<53) ; code segment
+.pointer:
+    dw $ - gdt64 - 1
+    dq gdt64
