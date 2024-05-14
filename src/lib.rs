2e75d7eec2746df00a82ffa815ac0262718565f1
Joseph LANZA <jlanza@student.42.fr>
2024-05-14 15:47:10 +0200

    create print memory function

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ pub extern fn k_main() {
 /* ************************************************************************** */
 \n\n\n\n\n\n\n\n\n\n\n\n");
     gdt::init_gdt();
+    print_mem_area(0x800 as *mut i32, 24*4);
     loop{}
 }
 
@@ -33,4 +34,44 @@ pub extern fn k_main() {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+fn print_mem_area(addr: *mut i32, size: usize) {
+    for number in 0..(size / 4) {
+        print_mem_line(addr.wrapping_add(number * 4));
+    }
+}
+
+fn print_mem_line(addr: *mut i32) {
+    print!("{:010?}   |   ", addr);
+
+    unsafe {
+        print!("{:08x}  ", *(addr.wrapping_add(0)) as u32);
+        print!("{:08x}  ", *(addr.wrapping_add(1)) as u32);
+        print!("{:08x}  ", *(addr.wrapping_add(2)) as u32);
+        print!("{:08x}  ", *(addr.wrapping_add(3)) as u32);
+        println!("");
+    }
+}
+
+fn fill_memory() {
+    let addr: *mut i32 = 0x800 as *mut i32;
+    unsafe {
+        let addr2: *mut i32 = 0x800 as *mut i32;
+        *(addr2) = 0x1u32 as i32;
+        let addr2 = 0x804 as *mut i32;
+        *(addr2) = 0x2u32 as i32;
+        let addr2 = 0x808 as *mut i32;
+        *(addr2) = 0x3u32 as i32;
+        let addr2 = 0x80c as *mut i32;
+        *(addr2) = 0x4u32 as i32;
+        let addr2 = 0x810 as *mut i32;
+        *(addr2) = 0x5u32 as i32;
+
+        let addr3: *mut i64 = 0x830 as *mut i64;
+        *(addr3) = 0xFF_FFFF_FFFFu64 as i64;
+
+        let addr2 = 0x838 as *mut i32;
+        *(addr2) = 0x42u32 as i32;
+    }
 }
