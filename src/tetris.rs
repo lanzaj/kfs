74cf3606e3ec097ef97d283b0798a4e34f683073
lanzaj <jlanza@student.42.fr>
2024-05-25 04:48:12 +0200

    rotation and horizontal move ok

--- src/tetris.rs
+++ src/tetris.rs
@@ -104,16 +104,16 @@ fn  draw_rectangle(row_up: usize, row_down: usize, col_left:usize, col_right:usi
 }
 
 fn draw_text() {
-    draw_str(1, 37, "Tetris", Color::White, Color::Black);
-    draw_str(1, 16, "Stats", Color::White, Color::Black);
-    draw_str(1, 60, "Next", Color::White, Color::Black);
-    draw_str(8, 60, "Help", Color::White, Color::Black);
-    draw_str(2, 11, "Score", Color::White, Color::Black);
-    draw_str(4, 11, "Level", Color::White, Color::Black);
+    draw_str(2, 37, "Tetris", Color::White, Color::Black);
+    draw_str(2, 16, "Stats", Color::White, Color::Black);
+    draw_str(2, 60, "Next", Color::White, Color::Black);
+    draw_str(9, 60, "Help", Color::White, Color::Black);
+    draw_str(3, 11, "Score", Color::White, Color::Black);
+    draw_str(5, 11, "Level", Color::White, Color::Black);
 }
 
 fn draw_bg() {
-    for row in 2..24 {
+    for row in 3..23 {
         for col in (31..51).step_by(2) {
             draw_char(row, col, '.' as u8, Color::DarkGray, Color::Black);
         }
@@ -122,7 +122,7 @@ fn draw_bg() {
 
 fn draw_cell(x:usize, y: usize, color: Color) {
     let col = x * 2 + 30;
-    let row = 23 - y;
+    let row = 22 - y;
 
     for i in 0..2 {
         draw_char(row, col + i, 32, color, color);
@@ -131,87 +131,92 @@ fn draw_cell(x:usize, y: usize, color: Color) {
 
 fn draw_empty_cell(x:usize, y: usize, color: Color) {
     let col = x * 2 + 30;
-    let row = 23 - y;
-    draw_char(row, col + 1, '.' as u8, color, color);
+    let row = 22 - y;
+    draw_char(row, col, ' ' as u8, Color::Black, Color::Black);
+    draw_char(row, col + 1, '.' as u8, color, Color::Black);
 }
 
 fn draw_next(tetraminos: char) {
     match tetraminos {
         'I' => {
-            draw_cell(14, 19, Color::Cyan);
-            draw_cell(15, 19, Color::Cyan);
-            draw_cell(16, 19, Color::Cyan);
-            draw_cell(17, 19, Color::Cyan);
+            draw_cell(14, 18, Color::Cyan);
+            draw_cell(15, 18, Color::Cyan);
+            draw_cell(16, 18, Color::Cyan);
+            draw_cell(17, 18, Color::Cyan);
         }
         'J' => {
-            draw_cell(15, 20, Color::Blue);
-            draw_cell(15, 19, Color::Blue);
-            draw_cell(16, 19, Color::Blue);
-            draw_cell(17, 19, Color::Blue);
+            draw_cell(15, 18, Color::Blue);
+            draw_cell(15, 17, Color::Blue);
+            draw_cell(16, 17, Color::Blue);
+            draw_cell(17, 17, Color::Blue);
         }
         'L' => {
-            draw_cell(15, 19, Color::Brown);
-            draw_cell(16, 19, Color::Brown);
-            draw_cell(17, 19, Color::Brown);
-            draw_cell(17, 20,Color::Brown);
+            draw_cell(15, 17, Color::Brown);
+            draw_cell(16, 17, Color::Brown);
+            draw_cell(17, 17, Color::Brown);
+            draw_cell(17, 18,Color::Brown);
         }
         'O' => {
-            draw_cell(15, 19, Color::Yellow);
-            draw_cell(15, 20, Color::Yellow);
-            draw_cell(16, 19, Color::Yellow);
-            draw_cell(16, 20,Color::Yellow);
+            draw_cell(15, 17, Color::Yellow);
+            draw_cell(15, 18, Color::Yellow);
+            draw_cell(16, 17, Color::Yellow);
+            draw_cell(16, 18,Color::Yellow);
         }
         'S' => {
-            draw_cell(15, 19, Color::LightGreen);
-            draw_cell(16, 19, Color::LightGreen);
-            draw_cell(16, 20,Color::LightGreen);
-            draw_cell(17, 20, Color::LightGreen);
+            draw_cell(15, 17, Color::LightGreen);
+            draw_cell(16, 17, Color::LightGreen);
+            draw_cell(16, 18,Color::LightGreen);
+            draw_cell(17, 18, Color::LightGreen);
         }
         'Z' => {
-            draw_cell(15, 20, Color::Red);
-            draw_cell(16, 19, Color::Red);
-            draw_cell(16, 20,Color::Red);
-            draw_cell(17, 19, Color::Red);
+            draw_cell(15, 18, Color::Red);
+            draw_cell(16, 17, Color::Red);
+            draw_cell(16, 18,Color::Red);
+            draw_cell(17, 17, Color::Red);
         }
         'T' => {
-            draw_cell(15, 19, Color::Magenta);
-            draw_cell(16, 19, Color::Magenta);
-            draw_cell(16, 20,Color::Magenta);
-            draw_cell(17, 19, Color::Magenta);
+            draw_cell(15, 17, Color::Magenta);
+            draw_cell(16, 17, Color::Magenta);
+            draw_cell(16, 18,Color::Magenta);
+            draw_cell(17, 17, Color::Magenta);
         }
         _ => panic!("not a tetraminos"),
     }
 }
 
 fn draw_game_ui() {
-    draw_rectangle(1, 24, 29, 50);
-    draw_rectangle(1, 5, 10, 26);
-    draw_rectangle(1, 6, 53, 71);
-    draw_rectangle(8, 20, 53, 71);
+    draw_rectangle(2, 23, 29, 50);
+    draw_rectangle(2, 6, 10, 26);
+    draw_rectangle(2, 7, 53, 71);
+    draw_rectangle(9, 21, 53, 71);
     draw_text();
     draw_bg();
 
-    // for i in 0..10 {
-    //     if i % 2 == 0 {
-    //         draw_cell(i, 0, Color::Cyan);
-    //     }
-    //     else {
-    //         draw_cell(i, 0, Color::LightGreen)
-    //     }
-    // }
-    // for i in 0..22 {
-    //     if i % 2 == 0 {
-    //         draw_cell(0, i, Color::Cyan);
-    //     }
-    //     else {
-    //         draw_cell(0, i, Color::LightGreen)
-    //     }
-    // }
+    // draw_scale();
+    
+}
+fn  draw_scale() {
+    for i in 0..10 {
+        if i % 2 == 0 {
+            draw_cell(i, 0, Color::Cyan);
+        }
+        else {
+            draw_cell(i, 0, Color::LightGreen)
+        }
+    }
+    for i in 0..22 {
+        if i % 2 == 0 {
+            draw_cell(0, i, Color::Cyan);
+        }
+        else {
+            draw_cell(0, i, Color::LightGreen)
+        }
+    }
 }
 
 fn  draw_board(data: Data) {
     for x in 0..10 {
-        for y in 0..22 {
+        for y in 0..20 {
             match data.board[x][y] {
                 1 => draw_cell(x, y, Color::Cyan),
                 2 => draw_cell(x, y, Color::Blue),
@@ -220,7 +225,29 @@ fn  draw_board(data: Data) {
                 5 => draw_cell(x, y, Color::LightGreen),
                 6 => draw_cell(x, y, Color::Red),
                 7 => draw_cell(x, y, Color::Magenta),
-                _ => {},
+                _ => draw_empty_cell(x, y, Color::DarkGray),
+            }
+        }
+    }
+}
+
+fn  name_to_index(c: char) -> usize {
+    match c {
+        'I' => 0,
+        'J' => 1,
+        'L' => 2,
+        'O' => 3,
+        'S' => 4,
+        'Z' => 5,
+        'T' => 6,
+        _ => 7,
+    }
+}
+fn  draw_current_tetrominos(data: Data) {
+    for y in 0..4 {
+        for x in 0..4 {
+            if rot_array[name_to_index(data.current)][data.rot][y][x] != 0 {
+                draw_cell((data.pos.x + x as i32)as usize, (data.pos.y + 4 - y as i32) as usize, data.col)
             }
         }
     }
@@ -228,9 +255,16 @@ fn  draw_board(data: Data) {
 
 fn  display_game(data: Data) {
     draw_board(data);
-    draw_nbr(2, 24, data.score, Color::White, Color::Black);
-    draw_nbr(4, 24, data.level, Color::White, Color::Black);
+    draw_nbr(3, 24, data.score, Color::White, Color::Black);
+    draw_nbr(5, 24, data.level, Color::White, Color::Black);
     draw_next(data.next);
+    draw_current_tetrominos(data);
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Coord {
+    x: i32,
+    y: i32,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -242,8 +276,9 @@ struct Data {
     scan_code: u8,
     next: char,
     current: char,
-    pos: [u8; 2],
-    rot: u8,
+    pos: Coord,
+    rot: usize,
+    col: Color,
 }
 
 impl Data {
@@ -254,20 +289,51 @@ impl Data {
             level: 1,
             score: 0,
             scan_code: 0,
-            next: 'I',
-            current: 'L',
-            pos: [0; 2],
+            next: 'T',
+            current: 'T',
+            pos: Coord { x: 3, y: 15 },
             rot: 0,
+            col: Color::Magenta,
         }
     }
 }
 
 use crate::io::read_data;
 
-fn handle_keyboard_input(data: &mut Data) {
+fn  check_cell(data: Data) -> bool {
+    for y in 0..4 {
+        for x in 0..4 {
+            if rot_array[name_to_index(data.current)][data.rot][y][x] != 0 &&
+                data.pos.x + x >= 0 &&
+                data.pos.x + x < 10 &&
+                data.pos.y + y >= 0 &&
+                data.pos.y + y < 22 {
+                    return true
+            }
+        }
+    }
+}
+
+fn  handle_keyboard_input(data: &mut Data) {
     if data.scan_code == 1 {
         data.end = true;
         return;
+    }
+    if data.scan_code == 37 {
+        data.rot = (data.rot + 1) % 4;
+    }
+    if data.scan_code == 36 {
+        data.rot = (data.rot + 3) % 4;
+    }
+    if data.scan_code == 30 {
+        if data.pos.x - 1 > 0 {
+            data.pos.x = data.pos.x - 1;
+        }
+    }
+    if data.scan_code == 32 {
+        if data.pos.x + 1 < 10 {
+            data.pos.x = data.pos.x + 1;
+        }
     }
 }
 
@@ -283,15 +349,19 @@ fn  fill_fake_board(data: &mut Data) {
     data.board[5][1] = 7;
 }
 
+fn  exit_tetris() {
+    WRITER.lock().toggle_cmd(true);
+    println!("See you soon!");       
+}
+
 pub fn ft_tetris() {
     let mut data: Data = Data::new();
     clear_window();
     draw_game_ui();
-    fill_fake_board(&mut data);
+    //fill_fake_board(&mut data);
     loop {
         if data.end {
-            WRITER.lock().toggle_cmd(true);
-            println!("See you soon!                                                            ");
+            exit_tetris();
             break;
         }
         data.scan_code = read_data();
@@ -300,187 +370,187 @@ pub fn ft_tetris() {
     }
 }
 
-// let array: [[[[u8; 4]; 4]; 4]; 7] = [
-//         [
-//             [
-//                 [0, 1, 2, 3],
-//                 [4, 5, 6, 7],
-//                 [8, 9, 10, 11],
-//                 [12, 13, 14, 15]
-//             ],
-//             [
-//                 [16, 17, 18, 19],
-//                 [20, 21, 22, 23],
-//                 [24, 25, 26, 27],
-//                 [28, 29, 30, 31]
-//             ],
-//             [
-//                 [32, 33, 34, 35],
-//                 [36, 37, 38, 39],
-//                 [40, 41, 42, 43],
-//                 [44, 45, 46, 47]
-//             ],
-//             [
-//                 [48, 49, 50, 51],
-//                 [52, 53, 54, 55],
-//                 [56, 57, 58, 59],
-//                 [60, 61, 62, 63]
-//             ]
-//         ],
-//         [
-//             [
-//                 [64, 65, 66, 67],
-//                 [68, 69, 70, 71],
-//                 [72, 73, 74, 75],
-//                 [76, 77, 78, 79]
-//             ],
-//             [
-//                 [80, 81, 82, 83],
-//                 [84, 85, 86, 87],
-//                 [88, 89, 90, 91],
-//                 [92, 93, 94, 95]
-//             ],
-//             [
-//                 [96, 97, 98, 99],
-//                 [100, 101, 102, 103],
-//                 [104, 105, 106, 107],
-//                 [108, 109, 110, 111]
-//             ],
-//             [
-//                 [112, 113, 114, 115],
-//                 [116, 117, 118, 119],
-//                 [120, 121, 122, 123],
-//                 [124, 125, 126, 127]
-//             ]
-//         ],
-//         [
-//             [
-//                 [128, 129, 130, 131],
-//                 [132, 133, 134, 135],
-//                 [136, 137, 138, 139],
-//                 [140, 141, 142, 143]
-//             ],
-//             [
-//                 [144, 145, 146, 147],
-//                 [148, 149, 150, 151],
-//                 [152, 153, 154, 155],
-//                 [156, 157, 158, 159]
-//             ],
-//             [
-//                 [160, 161, 162, 163],
-//                 [164, 165, 166, 167],
-//                 [168, 169, 170, 171],
-//                 [172, 173, 174, 175]
-//             ],
-//             [
-//                 [176, 177, 178, 179],
-//                 [180, 181, 182, 183],
-//                 [184, 185, 186, 187],
-//                 [188, 189, 190, 191]
-//             ]
-//         ],
-//         [
-//             [
-//                 [192, 193, 194, 195],
-//                 [196, 197, 198, 199],
-//                 [200, 201, 202, 203],
-//                 [204, 205, 206, 207]
-//             ],
-//             [
-//                 [208, 209, 210, 211],
-//                 [212, 213, 214, 215],
-//                 [216, 217, 218, 219],
-//                 [220, 221, 222, 223]
-//             ],
-//             [
-//                 [224, 225, 226, 227],
-//                 [228, 229, 230, 231],
-//                 [232, 233, 234, 235],
-//                 [236, 237, 238, 239]
-//             ],
-//             [
-//                 [240, 241, 242, 243],
-//                 [244, 245, 246, 247],
-//                 [248, 249, 250, 251],
-//                 [252, 253, 254, 255]
-//             ]
-//         ],
-//         [
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ],
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ],
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ],
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ]
-//         ],
-//         [
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ],
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ],
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ],
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ]
-//         ],
-//         [
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ],
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ],
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ],
-//             [
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0],
-//                 [0, 0, 0, 0]
-//             ]
-//         ]
-//     ];
+const rot_array: [[[[u8; 4]; 4]; 4]; 7] = [
+        [ // I
+            [
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 1, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0]
+            ]
+        ],
+        [ // J
+            [
+                [2, 0, 0, 0],
+                [2, 2, 2, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 2, 2, 0],
+                [0, 2, 0, 0],
+                [0, 2, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [2, 2, 2, 0],
+                [0, 0, 2, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 2, 0, 0],
+                [0, 2, 0, 0],
+                [2, 2, 0, 0],
+                [0, 0, 0, 0]
+            ]
+        ],
+        [ // L
+            [
+                [0, 0, 3, 0],
+                [3, 3, 3, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 3, 0, 0],
+                [0, 3, 0, 0],
+                [0, 3, 3, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [3, 3, 3, 0],
+                [3, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [3, 3, 0, 0],
+                [0, 3, 0, 0],
+                [0, 3, 0, 0],
+                [0, 0, 0, 0]
+            ]
+        ],
+        [ // O
+            [
+                [0, 4, 4, 0],
+                [0, 4, 4, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 4, 4, 0],
+                [0, 4, 4, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 4, 4, 0],
+                [0, 4, 4, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 4, 4, 0],
+                [0, 4, 4, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ]
+        ],
+        [ // S
+            [
+                [0, 5, 5, 0],
+                [5, 5, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 5, 0, 0],
+                [0, 5, 5, 0],
+                [0, 0, 5, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 5, 5, 0],
+                [5, 5, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [5, 0, 0, 0],
+                [5, 5, 0, 0],
+                [0, 5, 0, 0],
+                [0, 0, 0, 0]
+            ]
+        ],
+        [ // Z
+            [
+                [6, 6, 0, 0],
+                [0, 6, 6, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 6, 0],
+                [0, 6, 6, 0],
+                [0, 6, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [6, 6, 0, 0],
+                [0, 6, 6, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 6, 0, 0],
+                [6, 6, 0, 0],
+                [6, 0, 0, 0],
+                [0, 0, 0, 0]
+            ]
+        ],
+        [ // T
+            [
+                [0, 7, 0, 0],
+                [7, 7, 7, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 7, 0, 0],
+                [0, 7, 7, 0],
+                [0, 7, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [7, 7, 7, 0],
+                [0, 7, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 7, 0, 0],
+                [7, 7, 0, 0],
+                [0, 7, 0, 0],
+                [0, 0, 0, 0]
+            ]
+        ]
+    ];
